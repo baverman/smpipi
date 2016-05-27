e53526c50ea40b0cd6299c6e561c3fbd4a88d0e4
Anton Bobrov <bobrov@vl.ru>
2016-05-27 18:29:13 +0300

    post_handler

--- smpipi/esme.py
+++ smpipi/esme.py
@@ -10,11 +10,12 @@ pdu_log = logging.getLogger('smpipi.pdu')
 
 class ESME(object):
     def __init__(self, host, port, timeout=60, handler=None,
-                 enquire_timeout=300, conn_timeout=10):
+                 enquire_timeout=300, conn_timeout=10, post_handler=None):
         self.sequence_number = 0
         self._socket = socket.create_connection((host, port), timeout=conn_timeout)
         self._socket.settimeout(timeout)
         self.handler = handler
+        self.post_handler = post_handler
         self.enquire_timeout = 300
         self.last_enquire = time.time()
 
@@ -55,9 +56,10 @@ class ESME(object):
             raise StopIteration()
         elif cmd_type is command.DeliverSM:
             resp = command.DeliverSMResp(**seq)
-            if self.handler:
-                self.handler(cmd, resp)
+            result = self.handler and self.handler(cmd, resp)
             self.reply(resp)
+            if self.post_handler:
+                self.post_handler(result)
         else:
             raise Exception('Unknown command: {}'.format(cmd))
 
